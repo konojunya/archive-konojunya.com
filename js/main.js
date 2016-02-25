adfae3b780472e3790c759f4813bd3fd1b005c26
konojunya <junya03dance@gmail.com>
2016-02-26 02:47:13 +0900

    メニューを左から出るUIに変更

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 (function() {
   $(function() {
     window.onload = function() {
-      var _ua;
+      var _ua, bool;
       _ua = (function(u) {
         return {
           Tablet: u.indexOf('windows') !== -1 && u.indexOf('touch') !== -1 || u.indexOf('ipad') !== -1 || u.indexOf('android') !== -1 && u.indexOf('mobile') === -1 || u.indexOf('firefox') !== -1 && u.indexOf('tablet') !== -1 || u.indexOf('kindle') !== -1 || u.indexOf('silk') !== -1 || u.indexOf('playbook') !== -1,
@@ -16,9 +16,10 @@
         target = $(href === '#' || href === '' ? 'html' : href);
         position = target.offset().top;
         if (_ua.Mobile || _ua.Tablet) {
-          $('.navi').fadeOut();
-          $('fa-remove').hide();
-          $('.fa-bars').fadeIn();
+          $(".btn").attr("class","fa fa-bars fa-2x btn");
+          $(".navi").animate({
+            left: "-100%"
+          },300);
         }
         $('html, body').animate({
           scrollTop: position
@@ -33,22 +34,25 @@
       setTimeout((function() {
         $('.topAni').fadeOut('slow');
         return this;
-      }), 5 * 1000);
-      $('.fa-bars').on('click', function() {
-        $('.navi').fadeIn();
-        $(this).hide();
-        $('.fa-remove').fadeIn();
-        return this;
+      }), 5 * 1);
+      $('.btn').on('click', function() {
+        var clsArray;
+        clsArray = $(this).attr("class").split(" ");
+        if (clsArray[1] == "fa-bars") {
+          clsArray[1] = "fa-remove";
+          $(this).attr("class", clsArray.join(" "));
+          $(".navi").animate({
+            left: "0%"
+          },300);
+        } else if (clsArray[1] == "fa-remove") {
+          clsArray[1] = "fa-bars";
+          $(this).attr("class", clsArray.join(" "));
+          $(".navi").animate({
+            left: "-100%"
+          },300);
+        }
       });
-      $('.fa-remove').on('click', function() {
-        $('.navi').fadeOut();
-        $(this).hide();
-        $('.fa-bars').fadeIn();
-        return this;
-      });
-      return this;
     };
-    return this;
   });
 
 }).call(this);
