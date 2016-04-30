b80b3e634fbdf2af652a6dc8320502739f2da2a3
konojunya <junya03dance@gmail.com>
2016-05-01 01:07:05 +0900

    create scheduler add mdl

---
+++ contents/scheduler/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,user-scalable=no">
+
+  <link rel="stylesheet" href="stylesheets/lib/sanitize.min.css">
+  <link rel="stylesheet" href="stylesheets/lib/font-awesome.min.css">
+  <link rel="stylesheet" href="stylesheets/lib/material.min.css">
+  <link rel="stylesheet" href="stylesheets/src/style.css">
+
+  <title>scheduler</title>
+
+</head>
+
+<body>
+
+
+
+	<script src="./javascripts/lib/jquery.min.js"></script>
+	<script src="./javascripts/lib/material.min.js"></script>
+	<script src="./javascripts/src/index.js"></script>
+</body>
+
+</html>
