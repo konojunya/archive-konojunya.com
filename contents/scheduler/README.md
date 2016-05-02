ffbe8b839d42c102df5489d074732198fbb5b2c6
konojunya <junya03dance@gmail.com>
2016-05-03 01:16:45 +0900

    add readme.md

---
+++ contents/scheduler/README.md
@@ -0,0 +1,42 @@
+#HAL Schedules
+
+##Models
+
+- User Model
+
+|Property |Type     |
+|:---------|:--------:|
+|id       |String 	|
+|password |String 	|
+|class    |String 	|
+
+
+- Schedules Model
+
+|Property			|Type		|
+|:------------|:-----:|
+|monday				|Object	|
+|tuesday			|Object	|
+|wednesday		|Object	|
+|thursday			|Object	|
+|friday				|Object	|
+|createUser		|String	|
+|ScheduleName	|String	|
+
+|Object		|Type	 |
+|:--------|:----:|
+|prefix		|String|
+|subject	|String|
+|roomNum	|String|
+
+##API
+
+|Method	|URL             |Memo                                                                      											|
+|:------|:--------------:|:-----------------------------------------------------------------------------------------------|
+|GET    |/               |クッキーを見て、index or main                                                     								|
+|POST   |/login          |/からのidをもとにデータベース参照。クッキーをもたせてredirect / or /signup                         |
+|GET    |/signup         |新規登録。signup.ectを返す                                                        								|
+|POST   |/singup         |id.password.classを見てidはユニークなので、すでにあればsignup.ectなければsignup-addschedule.ect 		|
+|POST   |/addSchedules   |siignup-addからのデータを保存→クッキーをもたせてredirect / or 保存できなかったらsignup-addを返す		|
+|GET    |/schedules      |その人の追加したスケジュールが一覧で出てくる(クッキー確認)<-ダメなら redirect /                     |
+|GET    |/schedules/uuid |アプリのメイン画面                                                                 								|
--- contents/scheduler/app.js
+++ contents/scheduler/app.js
@@ -35,23 +35,47 @@ app.get('/', function (req, res) {
 		res.render("index.ect");
 	}
 });
+
 app.get('/signup', function (req, res) {
   res.render("signup.ect");
 });
+
 app.post("/addSchedules",function(req,res){
+	var user = new User();
+	user.id = req.body.id;
+	user.password = req.body.password;
+	user.save(function(err){
+		if(err)
+			res.send(err);
+	});
 	res.render("signup-schedule.ect");
 });
 app.get("/addSchedules",function(req,res){
 	res.redirect("/signup");
-})
+});
+
 app.post("/login",function(req,res){
-	if(req.body.id == "konojunya"){
-		res.cookie('token', req.body.id, {maxAge:6000, httpOnly:true});
+	var inputId = req.body.id;
+	User.find({},{_id: false,id: true},function(err,users){
+		for(var i = 2; i < users.length; i++){
+			if(users[i].id == inputId){
+				res.cookie('token', inputId, {maxAge: 6000, httpOnly:true});
+				res.redirect("/");
+			}else{
+				res.redirect("/signup");
+			}
+		}
+	});
+	res.redirect("/signup");
+});
+app.get("/login",function(req,res){
+	if(req.cookies.token){
 		res.redirect("/");
 	}else{
 		res.redirect("/signup");
 	}
 });
+
 app.post("/save",function(req,res){
 	if(req.body.end){
 		res.cookie('token', req.body.id, {maxAge:10000, httpOnly:true});
@@ -61,6 +85,24 @@ app.post("/save",function(req,res){
 app.get("/save",function(req,res){
 	res.redirect("/");
 });
-
+// app.get("/test",function(req,res){
+// 	TestUser.find(function(err,users){
+// 		if(err)
+// 			res.send(err);
+// 		console.log(users);
+// 		res.send(users);
+// 	});
+// });
+// app.post("/test",function(req,res){
+// 	var user = new TestUser();
+// 	user.name = req.body.id;
+// 	user.password = req.body.password;
+// 	user.save(function(err){
+// 		if(err)
+// 			res.send(err);
+// 		console.log("add user!");
+// 	});
+// 	res.redirect("/");
+// });
 server.listen(port);
 console.log('listening on port: ' + port);
