6efefe8fcd4a2ddaedd0ec9abf4d7e82c78c1904
konojunya <junya03dance@gmail.com>
2017-07-16 01:21:00 +0900

    add meta

--- src/javascripts/components/Base.jsx
+++ src/javascripts/components/Base.jsx
@@ -8,9 +8,22 @@ export default class Base extends React.Component {
     return(
       <html>
         <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link rel="stylesheet" href="style.css"/>
-        <title>konojunya.com/README.md</title>
+          <meta name="description" content="大阪でフロントエンドエンジニアとして活動しているHAL大阪の学生です！基本的にはJSでコソコソしています。" />
+          <meta name="keyword" content="konojunya,konojunya.com,engineer,osaka,hal,website,javascript" />
+          <meta property="og:title" content="konojunya.com/README.md" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="http://www.konojunya.com" />
+          <meta property="og:image" content="http://www.konojunya.com/ogp-image.png" />
+          <meta property="og:site_name" content="konojunya.com/README.md" />
+          <meta property="og:description" content="大阪でフロントエンドエンジニアとして活動しているHAL大阪の学生です！基本的にはJSでコソコソしています。" />
+          <meta property="fb:app_id" content="100007518556907" />
+          <meta name="twitter:card" content="summarylargeimage" />
+          <meta name="twitter:site" content="@konojunya" />
+          <meta name="twitter:player" content="@konojunya" />
+          <meta property="og:email" content="junya.developer@gmail.com" />
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+          <link rel="stylesheet" href="style.css"/>
+          <title>konojunya.com/README.md</title>
         </head>
         <body>
           <App/>
